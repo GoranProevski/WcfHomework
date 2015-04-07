882ee5d54db797009adfb74b04f3d7177d8c8c69
GoranProevski <goran.proevski@gmail.com>
2015-04-07 20:54:43 +0200

    training homework

---
+++ TestWcf/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestWcf.SvcCars;
+
+namespace TestWcf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+
+            SvcCars.CarServiceClient client = new SvcCars.CarServiceClient();
+
+            Car[] listCars = null;
+
+            listCars = client.GetAllCars();
+
+            dataGridView1.DataSource = listCars;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = null;
+            SvcCars.CarServiceClient client = new SvcCars.CarServiceClient();
+
+            Car[] listCars = null;
+
+            listCars = client.GetCarById(textBox1.Text);
+
+            dataGridView1.DataSource = listCars;
+            
+        }
+    }
+}
---
+++ WcfHomework/CarService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Xml.Linq;
+
+namespace WcfHomework
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class CarService : ICarService
+    {
+
+        public List<Car> GetAllCars()
+        {
+            XDocument db = GetDb();
+            List<Car> lstCars =
+                (from car in db.Descendants("car")
+                 select new Car()
+                 {
+                     ID = car.Attribute("id").Value,
+                     Make = car.Element("make").Value,
+                     Model = car.Element("model").Value,
+                     Doors = car.Element("doors").Value,
+                     Price = Convert.ToDecimal(car.Element("price").Value)
+                 }).ToList();
+
+            return lstCars;
+        }
+
+        public List<Car> GetCarById(string id)
+        {
+            XDocument db = GetDb();
+
+            List<Car> lstCar =
+                (from car in db.Descendants("car")
+                 where car.Attribute("id").Value.Equals(id)
+                 select new Car()
+                 {
+                     ID = car.Attribute("id").Value,
+                     Make = car.Element("make").Value,
+                     Model = car.Element("model").Value,
+                     Doors = car.Element("doors").Value,
+                     Price = Convert.ToDecimal(car.Element("price").Value)
+                 }).ToList();
+
+            return lstCar;
+        }
+
+        /// <summary>
+        /// metod za loadiranje na podatocite od xml fajlot Storage.xml
+        /// treba da se vnese apsolutnata pateka do lokacijata kade e fajlot
+        /// </summary>
+        /// <returns></returns>
+        private XDocument GetDb()
+        {
+            //return XDocument.Load(@"..\..\Storage.xml");
+            return XDocument.Load(@"c:\users\gp\documents\visual studio 2013\Projects\WcfHomework\WcfHomework\Storage.xml");
+        }
+    }
+}
